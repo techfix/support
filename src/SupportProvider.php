6b650a025b72585f2ef71b491a4ccba737c6d0fd
Derek Bourgeois <derek@ibourgeois.com>
2018-09-29 18:18:02 -0400

    big bang

---
+++ src/SupportProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TechFix\Support;
+
+use Illuminate\Support\ServiceProvider;
+
+class SupportProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
